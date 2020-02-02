1e8ba497360581ef0409ca31853c0871ca44604d
Buhungiro Caleb <caleb.buhungiro@finology.com.my>
2020-02-02 14:06:27 +0800

    add firstname and lastname

--- person.js
+++ person.js
@@ -1,12 +1,14 @@
 class Person {
-  name;
-  country;
-  constructor(name, country) {
-    this.name = name;
+  constructor(firstName, lastName, country) {
+    // if (firstName === undefined || lastName === undefined || country === undefined){
+    //   throw "please provide firstName, lastName, country";
+    // }
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.country = country;
   }
   introduce() {
-    return `My name is ${this.name} and my country is ${this.country}`;
+    return `My name is ${this.firstName} and my country is ${this.country}`;
   }
 }
 module.exports = Person;
